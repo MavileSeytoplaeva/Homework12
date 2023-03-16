8494201056c3361559f97f54b01bf084eda4420f
MavileSeytoplaeva <mavile.seitoplaeva@gmail.com>
2023-03-16 21:38:52 +0300

    Домашка доработана

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.time.LocalDate;
 import java.util.Scanner;
 import java.lang.String;
+import static java.lang.System.in;
 
 public class Main {
 
     public static void main(String[] args) {
 //        task1();
 //        task2();
+
         task3();
         task3();
         task3();
         task3();
+
     }
 
-    public static String checkYear(int AYear) {
-        if (AYear % 4 == 0 && AYear % 100 != 1 || AYear % 400 == 0) {
+    public static String checkLeapYear(int AYear) {
+        if (AYear % 4 == 0 && AYear % 100 != 1 && AYear % 400 == 0) {
             return AYear+" год - високосный год.";
         } else {
             return AYear+" год - обычный год.";
@@ -25,10 +26,12 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(in);
         int year = scanner.nextInt();
-        String checking = checkYear(year);
+        String checking = checkLeapYear(year);
         System.out.println(checking);
+        scanner.close();
+
     }
 
     public static String checkDeviceYearAndVersion(int year, int version, int currentYear) {
@@ -52,31 +55,35 @@ public class Main {
 
     public static void task2() {
         System.out.println("Задача 2");
-        char clientOS = 1;
-        short clientDeviceYear = 2012;
+        int clientOS = 1;
+        int clientDeviceYear = 2012;
         int currentYear = LocalDate.now().getYear();
         String checked = checkDeviceYearAndVersion(clientDeviceYear, clientOS, currentYear);
         System.out.println(checked);
     }
 
-    public static String checkDelivery(int distance){
-        int deliveryDays = 1;
-        if (distance <= 20) {
-            return "Требуется дней: " + deliveryDays;
-        } else if (distance > 20 && distance < 100) {
+    public static String checkDeliveryDays(int distance) {
+//        int distance = 23;
+        int deliveryDays = 0;
+        if (distance > 0 && distance < 100) {
             deliveryDays++;
-            if (distance > 60) {
+            if (distance > 20) {
                 deliveryDays++;
+                if (distance > 60) {
+                    deliveryDays++;
+                }
             }
-            return "Требуется дней: " + deliveryDays;
-        } else
+            return "Доставка дней: "+deliveryDays;
+        } else {
             return "Так далеко доставки нет.";
+        }
     }
-    public static void task3() {
-        System.out.println("Задача 3");
-        Scanner scanner = new Scanner(System.in);
-        int deliveryDistance = scanner.nextInt();
-        String checkedDelivery = checkDelivery(deliveryDistance);
-        System.out.println(checkedDelivery);
-    }
+        public static void task3 () {
+            System.out.println("Задача 3");
+            Scanner scanner = new Scanner(in);
+            int deliveryDistance = scanner.nextInt();
+            String checkedDeliveryDays = checkDeliveryDays(deliveryDistance);
+            System.out.println(checkedDeliveryDays);
+//            scanner.close();
+        }
 }
